c410f8be015033b11d31de77ec400e89d5979870
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-15 20:58:00 +0100

    update

--- credit_card.rb
+++ credit_card.rb
@@ -1,10 +1,30 @@
-str = gets.chomp
+puts "Enter a string: "
+length = gets.chomp.to_i
 
+def blur(tail = 4, char = '#')
+    if (Length <= tail)
+      self
+    else
+      char * (Length - tail) + self[-tail, tail]
+    end
+  end
+
+def mask(length)
+    length[0..-5] = '#' * (length.length - 4)
+    length
+end
+
+puts mask("#{length}")
+
+=begin
+puts "Enter a string: "
+str = gets.chomp
 i = 0
 count = 0
 while str[i]
+  print str[i]
   print str[i].replace "#" 
-  if str.length - count == 4
+  if str.length - count == 5
     while str[i]
       print str[i]
       i += 1
@@ -13,4 +33,4 @@ while str[i]
   i += 1
   count += 1
 end
-
+=end
