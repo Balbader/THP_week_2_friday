bfbf3f35fa127fd0ea4cab4ec716af908238f9ca
Youssef <annabiyoussef1986@gmail.com>
2021-01-15 15:34:31 +0100

    youssef energy

--- middle.rb
+++ middle.rb
@@ -1,9 +1,6 @@
+puts "YOUSSEF CODE"
 puts "Hello, please type a word and we'll give you the middle letters of that word"
 Answer = gets.chomp.to_s
-
-# count string#
-# even ? odd ?#
-# puts midde"
 
 def count_string
   if Answer.length.even?
